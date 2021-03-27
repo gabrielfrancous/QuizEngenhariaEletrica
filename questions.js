75224e60889efffb323f24e0c17c5df174a76cb8
gabrielfrancous <52275539+gabrielfrancous@users.noreply.github.com>
2021-03-27 12:26:09 -0700

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,62 @@
+let questions = [
+  {
+      question: 'Quais são as fases de resolução de um problema?',
+      answers: [
+        'Especificação->Indentificação de um problema->Formulação->Pesquisa->Análise->Decisão',
+        'Indentificação de um problema->Formulação->Análise->Pesquisa->Decisão->Especificação',
+        'Formulação->Análise->Indentificação de um problema->Pesquisa->Decisão->Especificação',
+        'Pesquisa->Especificação->Análise->Identifcação de um problema->Formulação'
+        ],
+      right: 'Indentificação de um problema->Formulação->Análise->Pesquisa->Decisão->Especificação'
+    },
+     {
+      question: 'Durante a fase de pesquisa por soluções alternativas, todos os fatores influenciam, exceto:',
+      answers: [
+        'O conhecimento prévio',
+        'A aptidão',
+        'O método',
+        'As aspirações pessoais'
+        ],
+      right: 'As aspirações pessoais'
+    },
+     {
+      question: 'No processo de formulação do problema, são três os atores envolvidos: o engenheiro, o cliente e o usuário. A função de detalhar o problema para que as possíveis soluções sejam vistas como satisfatórias é relativa ao:',
+      answers: [
+        'engenheiro',
+        'cliente',
+        'usuário',
+        'cliente e usuário'
+        ],
+      right: 'engenheiro'
+    },
+     {
+      question: 'Muito bem jovem garfanhoto, agora me diga. Para solucionar um problema que possuia alguma solução inicial mas por causa de outros fatores acabou não perdurando ,quais opções iniciais não devem ser feitas: I-Definir o problema de uma forma geral II-Evitar proucurar soluções durante a fase inicial III-Entrar em detalhes minuciosos IV-Aperfeiçoar a solução',
+      answers: [
+        'I e III',
+        'IV e III',
+        'II e I',
+        'II e III'
+        ],
+      right: 'IV e III'
+    },
+     {
+      question: 'Qual procedimento o engenheiro deve adotar na tomada de decisão para que as soluções sejam bem sucedidas:',
+      answers: [
+        'escolher critérios e a solução',
+        'prever o desempenho das soluções selecionadas',
+        'comparar alternativas conforme o desmpenho previsto',
+        'As alternativas anteriores estão corretas'
+        ],
+      right: 'As alternativas anteriores estão corretas'
+    },
+     {
+      question: 'Quais são os erros mais frequentes na definição de um problema?',
+      answers: [
+        'Aceitação do aspecto natural da solução anterior e generalização do problema',
+        'Determinar a forma mais economica de resolvê-lo e delimitar o problema',
+        'Generalização do problema e determinar a forma mais economica de resolve-lo',
+        'Delimitar o problema e aceitação do aspecto natural da solução anterior'
+        ],
+      right: 'Delimitar o problema e aceitação do aspecto natural da solução anterior'
+    },
+];
